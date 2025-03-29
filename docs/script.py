786155d09237c166aac57ad456b152cf118ec450
GVG <gabrielveronezgiolo@gmail.com>
2025-03-29 13:49:01 -0300

    botao reiniciar

--- docs/script.py
+++ docs/script.py
@@ -3,8 +3,19 @@ from browser import document, window
 def credito_ou_debito(event):
     window.location.assign("investir.html")
 
-def qual_produto(event):
+def comparar_produtos(event):
     window.location.assign("comparar.html")
 
-document["btn1"].bind("click", qual_produto)
-document["btn2"].bind("click", credito_ou_debito) 
+def reiniciar(event):
+    window.location.assign("index.html")
+
+def handle_click(event):
+    btn_id = event.target.id
+    if btn_id == "btn0":
+        reiniciar(event)
+    elif btn_id == "btn1":
+        comparar_produtos(event)
+    elif btn_id == "btn2":
+        credito_ou_debito(event)
+
+document.bind("click", handle_click) 
