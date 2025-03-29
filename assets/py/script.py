191c57a0f60beba43acb393c308e3c6a9527f8e4
GVG <gabrielveronezgiolo@gmail.com>
2025-03-29 12:42:38 -0300

    iniciado

--- assets/py/script.py
+++ assets/py/script.py
@@ -1,10 +1,10 @@
 from browser import document, window
 
 def credito_ou_debito(event):
-    window.location.assign("investir.html")
+    window.location.assign("pages/investir.html")
 
 def qual_produto(event):
-    window.location.assign("comparar.html")
+    window.location.assign("pages/comparar.html")
 
 document["btn1"].bind("click", qual_produto)
 document["btn2"].bind("click", credito_ou_debito) 
