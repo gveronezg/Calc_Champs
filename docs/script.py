4b2aee66c5a13524d9b99cfa5fb1e78d71626c3b
GVG <gabrielveronezgiolo@gmail.com>
2025-04-01 12:49:59 -0300

    finalizando

--- docs/script.py
+++ docs/script.py
@@ -16,17 +16,11 @@ def handle_click(event):
 def formatar_numero(event):
     input_element = event.target
     valor = input_element.value
-    
-    # Remove qualquer caractere que não seja número
-    valor = ''.join(filter(str.isdigit, valor))
-    
-    # Se o valor estiver vazio, limpa o input
-    if not valor:
+    valor = ''.join(filter(str.isdigit, valor)) # Remove qualquer caractere que não seja número
+    if not valor: # Se o valor estiver vazio, limpa o input
         input_element.value = ''
         return
-    
-    # Converte para float e divide por 100 para obter o valor em reais
-    try:
+    try: # Converte para float e divide por 100 para obter o valor em reais
         numero = float(valor) / 100
         input_element.value = f"{numero:.2f}"
     except:
@@ -48,11 +42,11 @@ def comparar_produtos(event):
     valorP2 = (unidP2 * pesoP2) / preçoP2
 
     if valorP1 > valorP2:
-        window.location.assign(f"resultado.html?resultado={preçoP1:.2f}")
+        window.location.assign(f"resultado.html?resultado={preçoP1:.2f}&tipo=1")
     elif valorP1 < valorP2:
-        window.location.assign(f"resultado.html?resultado={preçoP2:.2f}")
+        window.location.assign(f"resultado.html?resultado={preçoP2:.2f}&tipo=1")
     else:
-        window.location.assign("resultado.html?resultado=EMPATE")
+        window.location.assign(f"resultado.html?resultado={None}&tipo=2")
 
 def creditar_debitar(event):
     qtdPARC = float(document.getElementById("qtdPARC").value)
@@ -60,43 +54,61 @@ def creditar_debitar(event):
     vlrAVISTA = float(document.getElementById("vlrAVISTA").value)
     percREND = float(document.getElementById("percREND").value)
     total = qtdPARC * vlrPARC
-    montante = vlrAVISTA * (1 + percREND / 1) ** (1 * qtdPARC)
-    
+    montante = vlrAVISTA * (1 + percREND / 100) ** (1 * qtdPARC)
     montante = round(montante, 2)
     total = round(total, 2)
-    
-    # if qtdPARC <= 0 or vlrPARC <= 0 or vlrAVISTA <= 0 or percREND <= 0: # Verifica se os valores são válidos
 
     if montante > total:
         lucro = round(montante - total, 2)
-        resultado = f'''resultado.html?resultado=
-            Manter seus R${vlrAVISTA:.2f}
-            investidos a {percREND:.2f}% ao mês
-            te retornará o montante de R${montante:.2f}
-            ...
-            R${lucro:.2f} a mais do que os
-            R${total:.2f} pagos em {qtdPARC} meses.
-            '''
+        resultado = f"{vlrAVISTA},{percREND},{montante},{qtdPARC},{lucro},{total}"
+        window.location.assign(f"resultado.html?resultado={resultado}&tipo=3")
     elif montante < total:
         lucro = round(total - montante, 2)
-        resultado = f'''resultado.html?resultado=
-            Depois de {qtdPARC} meses
-            seu investimento teria retornado
-            apenas R${montante:.2f}
-            ...
-            R${lucro:.2f} é a economia que você
-            fará quitando esta divida agora!
-            '''
+        resultado = f"{qtdPARC},{montante},{lucro}"
+        window.location.assign(f"resultado.html?resultado={resultado}&tipo=4")
     else:
-        resultado = f'''vencedor.html?resultado=
-            Tanto faz pagar à vista ou parcelado.
-            '''
-    window.location.assign(resultado)
+        window.location.assign(f"resultado.html?resultado={None}&tipo=5")
+
+def formatar_resultado(resultado,tipo):
+    if tipo == "1":
+        return f'''O produto que sai por
+            R$ {resultado}
+            Vale mais a pena!
+        '''
+    elif tipo == "2":
+        return "Os 2 produtos tem o mesmo custo benefício!"
+    elif tipo == "3":
+        valores = [float(x) for x in resultado.split(',')]
+        return f'''Manter seus R${valores[0]:.2f} <br>
+            rentabilizando {valores[1]:.2f}% ao mês <br>
+            te retornará o montante de <br>
+            R${valores[2]:.2f} em {valores[3]:.0f} meses! <br>
+            ... <br>
+            R${valores[4]:.2f} a mais do que os <br>
+            R${valores[5]:.2f} pagos <br>
+            nestas {valores[3]:.0f} parcelas.
+        '''
+    elif tipo == "4":
+        valores = [float(x) for x in resultado.split(',')]
+        return f'''Depois de {valores[0]:.0f} meses <br>
+            seus investimentos <br>
+            teriam retornado apenas <br>
+            R${valores[1]:.2f} <br>
+            ... <br>
+            R${valores[2]:.2f} é a <br>
+            economia que você fará <br>
+            quitando esta divida agora!
+        '''
+    elif tipo == "5":
+        return "Tanto faz pagar à vista ou parcelado."
 
 # Verifica se estamos na página de resultado e mostra o resultado
 if 'resultado.html' in window.location.href:
     url = window.location.href
-    resultado = url.split('resultado=')[1]
-    document.getElementById('resultado').text = resultado
+    params = url.split('?')[1].split('&')
+    resultado = params[0].split('=')[1]
+    tipo = params[1].split('=')[1]
+    texto_formatado = formatar_resultado(resultado,tipo)
+    document.getElementById('resultado').innerHTML = texto_formatado
 
 document.bind("click", handle_click)
